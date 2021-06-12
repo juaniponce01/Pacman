a98c7aae36ce9c5f9b85f261696ae32609309307
juaniponce01 <62446939+juaniponce01@users.noreply.github.com>
2021-06-12 20:26:08 -0300

    "header Mapa"

--- tp4_2021_c1/src/Mapa.h
+++ tp4_2021_c1/src/Mapa.h
@@ -7,6 +7,33 @@
 
 
 class Mapa {
+public:
+    Mapa(Nat largo,
+         Nat alto,
+         Coordenada inicio,
+         Coordenada llegada,
+         set<Coordenada> paredes,
+         set<Coordenada> fantasmas,
+         set<Coordenada> chocolates)
+
+    Nat largo();
+    Nat alto();
+    Coordenada inicio();
+    Coordenada llegada();
+    vector<vector<string>> mapaDinamico();
+    set<Coordenada> chocolates();
+    set<Coordenada> paredes();
+    set<Coordenada> fantasmas();
+
+private:
+    Nat _alto;
+    Nat _largo;
+    Coordenada _inicio;
+    Coordenada _llegada;
+    vector<vector<string>> _mapaDinamico;
+    set<Coordenada> _chocolates;
+    set<Coordenada> _paredes;
+    set<Coordenada> _fantasmas;
 
 };
 
